72af84d4d09d7dca0b0908e7daff366ef34d5b32
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-01-24 19:28:36 -0400

    chore: add store directory and dummy db

---
+++ store/dummy.js
@@ -0,0 +1,33 @@
+const db = {
+  'users': [
+    { id: 1, name: 'Gustavo' },
+    { id: 2, name: 'Jesus' },
+    { id: 3, name: 'Martinez' },
+    { id: 4, name: 'Espezua' }
+  ]
+};
+
+function list(table) {
+  return db[table];
+}
+
+function get(table, id) {
+  const col = list(table);
+
+  return col.find((item) => item.id === id) || null;
+}
+
+function upsert(table, data) {
+  db[table].push(data);
+}
+
+function remove(table, id) {
+  return true;
+}
+
+module.exports = {
+  list,
+  get,
+  upsert,
+  remove
+}
