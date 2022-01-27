316a59e447816edc02094756df3161dbb58a55b1
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-01-26 20:44:01 -0400

    refactor(users): routes functions

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -5,16 +5,22 @@ const controller = require('./index');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+// Routes
+router.get('/', list);
+router.get('/:id', get);
+router.post('/', create);
+
+
+async function list (req, res) {
   try {
     const data = await controller.list();
     response.success(req, res, data, 200);
   } catch (error) {
     response.error(req, res, error.message, 500)
   }
-});
+}
 
-router.get('/:id', async (req, res) => {
+async function get(req, res) {
     try {
     const { id } = req.params;
     const user = await controller.get(id);
@@ -23,6 +29,17 @@ router.get('/:id', async (req, res) => {
   } catch (error) {
     response.error(req, res, error.message, 500)
   }
-});
+}
+
+async function create(req, res) {
+    try {
+    const body  = req.body;
+    const user = await controller.upsert(body);
+
+    response.success(req, res, user, 200);
+  } catch (error) {
+    response.error(req, res, error.message, 500)
+  }
+}
 
 module.exports = router;
