50df9ede56660c9129fe86c5cbac66042a6f496e
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-01-30 14:33:11 -0400

    feat(store): add get method to mysql connector

--- store/mysql.js
+++ store/mysql.js
@@ -49,6 +49,19 @@ function list(table) {
   })
 }
 
+function get(table, id) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM ${table} WHERE id='${id}'`, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(data);
+    });
+  })
+}
+
 module.exports = {
   list,
+  get,
 }
