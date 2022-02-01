3549d696ddcf32ec88d9a183e3506a4acb6edd73
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-01-31 21:09:43 -0400

    feat(auth): add check for follow user

--- api/components/user/secure.js
+++ api/components/user/secure.js
@@ -9,6 +9,11 @@ module.exports = function checkAuth(action) {
         auth.check.own(req, owner);
         next();
         break;
+      case 'follow':
+        auth.check.logged(req);
+        next();
+        break;
+
 
       default:
         next();
