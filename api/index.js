584dbe9be79cb987fb99a32c9b0c759e321e0411
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-01 17:27:38 -0400

    feat(api): add post to main app

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const config = require('./../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
+const post = require('./components/post/network');
 const errors = require('./../network/errors');
 // const swaggerDoc = require('./swagger.json');
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 // ROUTER
 app.use('/api/user', user);
 app.use('/api/auth', auth);
+app.use('/api/post', post);
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(errors);
