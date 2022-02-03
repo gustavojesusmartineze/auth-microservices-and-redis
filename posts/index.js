e9bc7d825d8b2fab9846c91b06d75f1b98084dbb
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-03 15:52:32 -0400

    feat(posts): make posts a microservice

---
+++ posts/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const config = require('./../config');
+const router = require('./components/post/network');
+const errors = require('./../network/errors');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/post', router);
+
+app.use(errors);
+
+
+app.listen(config.post.port, () => {
+  console.log('Posts service listening on port: ', config.post.port);
+});
