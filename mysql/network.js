664faa608a7d3b84dd708027ffff8af0319df3d5
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-02 20:16:51 -0400

    feat(mysql): add insert and update routes for mysql microservice

--- mysql/network.js
+++ mysql/network.js
@@ -7,6 +7,8 @@ const router = express.Router();
 
 router.get('/:table', list);
 router.get('/:table/:id', get);
+router.post('/:table', insert);
+router.put('/:table', update);
 
 async function list(req, res, next) {
   try {
@@ -21,6 +23,26 @@ async function list(req, res, next) {
 async function get(req, res, next) {
   try {
     const data = await Store.get(req.params.table, res.params.id);
+
+    response.success(req, res, data, 200);
+  } catch (error) {
+    next(error, req, res)
+  }
+}
+
+async function insert(req, res, next) {
+  try {
+    const data = await Store.insert(req.params.table, res.body);
+
+    response.success(req, res, data, 201);
+  } catch (error) {
+    next(error, req, res)
+  }
+}
+
+async function update(req, res, next) {
+  try {
+    const data = await Store.update(req.params.table, res.body);
 
     response.success(req, res, data, 200);
   } catch (error) {
