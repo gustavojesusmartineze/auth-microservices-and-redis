13ee7234a4bf969cd5be5d7334f4766f7df47507
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-02 20:16:21 -0400

    feat(mysql): add list and get routes for mysql microservice

---
+++ mysql/network.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const response = require('../network/response');
+const Store = require('../store/mysql');
+
+const router = express.Router();
+
+router.get('/:table', list);
+router.get('/:table/:id', get);
+
+async function list(req, res, next) {
+  try {
+    const data = await Store.list(req.params.table);
+
+    response.success(req, res, data, 200);
+  } catch (error) {
+    next(error, req, res)
+  }
+}
+
+async function get(req, res, next) {
+  try {
+    const data = await Store.get(req.params.table, res.params.id);
+
+    response.success(req, res, data, 200);
+  } catch (error) {
+    next(error, req, res)
+  }
+}
+
+module.exports = router;
