bfef91456a485dd4208784a5712d145d0c08221e
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-06 14:56:53 -0400

    chore: add chache service to pm2 ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,6 +28,20 @@ module.exports = {
       }
     },
     {
+      name: 'cache-service',
+      script: 'cache/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '2G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    },
+    {
       name: 'post-service',
       script: 'posts/index.js',
       instances: 1,
