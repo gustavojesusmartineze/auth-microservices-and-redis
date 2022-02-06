dde2e7c6e7ac80df3740730e475086182a646713
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-06 14:56:17 -0400

    feat(cache): improve network methods

--- cache/network.js
+++ cache/network.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const response = require('../network/response');
-const Store = require('../store/redis');
+const Store = require('./../store/redis');
 
 const router = express.Router();
 
@@ -10,18 +10,30 @@ router.get('/:table/:id', get);
 router.put('/:table', upsert);
 
 async function list(req, res, next) {
-  const data = await Store.list(req.params.table)
-  response.success(req, res, data, 200);
+  try {
+    const data = await Store.list(req.params.table);
+    response.success(req, res, data, 200);
+  } catch (error) {
+    next(error, req, res);
+  }
 }
 
 async function get(req, res, next) {
-  const data = await Store.get(req.params.table, req.params.id)
-  response.success(req, res, datos, 200);
+  try {
+    const data = await Store.get(req.params.table, req.params.id)
+    response.success(req, res, data, 200);
+  } catch (error) {
+    next(error, req, res);
+  }
 }
 
 async function upsert(req, res, next) {
-  const data = await Store.upsert(req.params.table, req.body)
-  response.success(req, res, datos, 200);
+  try {
+    const data = await Store.upsert(req.params.table, req.body)
+    response.success(req, res, data, 200);
+  } catch (error) {
+    next(error, req, res);
+  }
 }
 
 
