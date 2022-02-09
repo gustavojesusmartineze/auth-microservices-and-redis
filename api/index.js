124a7a8f10982c498acd5aae6e63605c27deae4a
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-08 21:07:16 -0400

    chore: refactor file locations for posts

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const config = require('./../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
-const post = require('./components/post/network');
 const errors = require('./../network/errors');
 // const swaggerDoc = require('./swagger.json');
 
@@ -16,7 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 // ROUTER
 app.use('/api/user', user);
 app.use('/api/auth', auth);
-app.use('/api/post', post);
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(errors);
